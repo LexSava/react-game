706c4544ad840aa370e83744bb14310e8270fd67
LexSava <lexsava@gmail.com>
2021-02-19 15:29:47 +0300

    feat: add playing field.
    Add playing field.
    Deketed eslint.

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+// import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [data, setData] = useState([
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+  ]);
+
+  // initialize 
+
+
+
+  // AddNumber 
+
+  return (<div style={{
+    background: '#aaaaaa',
+    width: "max-content",
+    margin: "auto",
+    padding: 5,
+    borderRadius: 5,
+    marginTop: 10,
+  }}>
+    {data.map((row, oneIndex) => {
+      return (
+        <div style={{ display: 'flex' }} key={oneIndex} >
+          {row.map((digit, index) => (
+            <Block num={digit} key={index} />
+          ))}
+        </div>
+      );
+    })
+    }</div>);
 }
+
+const Block = ({ num }) => {
+  const { blockStyle } = style;
+
+  return (<div style={{
+    ...blockStyle,
+    //background: getColor(num),
+    color: num === 2 || num === 4 ? "#645B52" : "#7c7a7a",
+  }}
+  >
+    {/* {num !== 0 ? num : ""} */}
+    {num}
+  </div>);
+}
+
+const style = {
+  blockStyle: {
+    height: 80,
+    width: 80,
+    background: "lightgray",
+    margin: 3,
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    fontSize: 45,
+    color: "white",
+    fontWeight: "600",
+  }
+};
 
 export default App;
